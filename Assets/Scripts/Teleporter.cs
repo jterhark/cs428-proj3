7c8aba2f630a3c9f0a744430aeb12933c104c1e1
jaket <jterha2@uic.edu>
2019-11-26 00:55:17 -0600

    teleport start

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.Locomotion.Teleporters;
+using Zinnia.Data.Type;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject target;
+    public TeleporterFacade facade;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Teleport()
+    {
+        facade.Teleport(new TransformData(target));
+        Console.WriteLine("Teleport Triggered");
+        
+    }
+
+}
